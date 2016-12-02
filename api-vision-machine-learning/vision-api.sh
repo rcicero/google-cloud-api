b7ff5eda991c6f3b81bd6672c4c6d1729d026323
rcicero <rafacicero.silva@gmail.com>
2016-12-02 18:46:35 -0200

    sns aws

--- api-vision-machine-learning/vision-api.sh
+++ api-vision-machine-learning/vision-api.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/bin/bash 
 
 #Image Path
 IMAGE_NAME=foto.png
 
+#SNS Topic
+SNSTOPIC={SNS URL}
+
 #Words
 declare -a words=();
+
+#SCORE MIN
+declare -i SCOPE_MIN=60
 
 takePicture(){
 	#Web Cam
@@ -15,7 +21,7 @@ takePicture(){
 callGoogleApi(){
 
 	#API KEY {Entrar no Google Cloud Console e gerar uma Credencial de Chave de API}
-	KEY={API GOOGLE}
+	KEY={API GOOGLE KEY}
 
 	#Montando a request -> parametros com base na doc Google Api Vision
 	REQUEST='{"requests":[{"image": {"content":"'$BASE64_IMAGE'"},"features":[{"type":"LABEL_DETECTION","maxResults":10}]}]}'
@@ -38,10 +44,13 @@ getWordsAndScoreInformation(){
 	ITENS=$(echo $RESPONSE | python -c "import json,sys;obj=json.load(sys.stdin);print len(obj['responses'][0]['labelAnnotations']);")
 	for (( count=0; count < $ITENS; ++count )); do
 		word=$(echo $RESPONSE | python -c "import json,sys;obj=json.load(sys.stdin);print obj['responses'][0]['labelAnnotations']["$count"]['description'];")
-		wordList[$count]=$word" "$(echo $RESPONSE | python -c "import json,sys;obj=json.load(sys.stdin);print obj['responses'][0]['labelAnnotations']["$count"]['score'];")
+		mod=${word//[ ]/_}
+		wordList[$count]=$mod":"$(echo $RESPONSE | python -c "import json,sys;obj=json.load(sys.stdin);print obj['responses'][0]['labelAnnotations']["$count"]['score'];")
+
 	done
 	echo ${wordList[*]}
 }
+
 
 learn(){
 
@@ -61,17 +70,17 @@ learn(){
 	#fi
 	> learning/right/$learnTitle
 	> learning/wrong/$learnTitle
-	sleep 2
+#	sleep 2
 	echo "Tudo preparado! Podemos começar."
-	sleep 2
+#	sleep 2
 	echo "Vou tirar uma foto e você me diz se é ou não o que espera"
-	sleep 4
+#	sleep 4
 	
 	while [ $menuOption = "s" ]; do
 
 		takePicture
 		callGoogleApi
-		words=$(getWordsAndScoreInformation)
+		words=$(getWordsInformation)
 
 		echo "Nesta foto eu encontrei -> ${words[*]}"
 		echo -n "Está correto (s/n)? "
@@ -93,9 +102,52 @@ learn(){
 		echo -n "Deseja continuar (s/n)? "
 		read menuOption
 	done
-	sleep 2
+#	sleep 2
 	echo "Ok. Vou processar os dados e finalizar o treinamento"
-	sleep 3
+#	sleep 2
+	#readarray results < learning/right/$learnTitle
+	#echo ${results[*]}	
+	#for result in $results ; do
+#		IFS=: read word score <<< $result
+#		qtd=$(grep -wR learning/right/$learnTitle -e $word)
+#		echo $word- $qtd -$score
+#
+#	done
+
+	echo ${ADDR[*]}
+
+}
+
+analyze(){
+
+	#TODO melhorar regra de avaliação
+
+	takePicture
+	callGoogleApi
+	words=$(getWordsInformation)
+	score=0	
+	wordsQtd=0
+	summer=1
+	wordsScore=0
+	for word in $words ; do
+		wordsQtd=$(( $wordsQtd + 1 ))
+		resposta=$(grep -o learning/right/$1 -e $word | wc -l)
+		if [[ $resposta -gt 0 ]]; then
+			score=$(( $score + 1 ))	
+		fi
+		
+	done
+
+	scoreResult=$(( $score * 100 ))
+	scoreResult=$(( $scoreResult / $wordsQtd ))
+
+	echo $scoreResult
+
+}
+
+sendEmail(){
+
+	aws sns publish --topic-arn $SNSTOPIC --message "Tem Uma Pessoa Aqui! :/"
 
 }
 
@@ -103,10 +155,14 @@ learn(){
 if [ "$1" = 'learn' ] ; then
 	learn
 elif [ "$1" = "analyze" ] ; then
-	takePicture
-	callGoogleApi
-	words=$(getWordsInformation)
-	echo ${words[*]}
+	analyzeScore=$(analyze $2)
+	if [[ $analyzeScore -ge $SCOPE_MIN ]]; then
+		#sendEmail
+		echo "É uma pessoa"
+	else
+		echo "Não é uma pessoa!"
+	fi
+
 else
 	echo "Comando não encontrado!"
 fi
